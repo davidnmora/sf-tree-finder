1df4e4014c6615028ff13b631ad54a878356f629
davidnmora <dmora1@stanford.edu>
2017-10-23 19:18:39 -0700

    Visualize all trees

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+// CITATION: drawTrees(data) based off cats-and-dogs-scatter in class
 console.clear();
 // Set up size
-let mapWidth = 750;
-let mapHeight = 750;
+const mapWidth = 750;
+const mapHeight = 750;
 
 // Set up projection that the map is using
-let projection = d3.geoMercator()
+const projection = d3.geoMercator()
   .center([-122.433701, 37.767683]) // San Francisco, roughly
   .scale(225000)
   .translate([mapWidth / 2, mapHeight / 2]);
@@ -15,26 +16,76 @@ let projection = d3.geoMercator()
 
 // Add an SVG element to the DOM
 let svg = d3.select('body').append('svg')
-  .attr('width', mapWidth)
-  .attr('height', mapHeight);
+.attr('width', mapWidth)
+.attr('height', mapHeight);
 
 // Add SVG map at correct size, assuming map is saved in a subdirectory called `data`
 svg.append('image')
-  .attr('width', mapWidth)
-  .attr('height', mapHeight)
-  .attr('xlink:href', 'https://magrawala.github.io/cs448b-fa17/assets/a3/sf-map.svg');
+.attr('width', mapWidth)
+.attr('height', mapHeight)
+.attr('xlink:href', 'https://magrawala.github.io/cs448b-fa17/assets/a3/sf-map.svg');
+
+
 
 // load data
-var dataset = []
-d3.csv("https://magrawala.github.io/cs448b-fa17/assets/a3/trees.csv", function(data) {
-   dataset = data.map(function(d) { 
-     
-     return d; });
-   console.log(dataset)
+d3.csv('data/trees.csv', function(data) {
+ let dataset = [];
+ data.forEach(d => { 
+  if (unususable(d)) return;
+  else dataset.push(clean(d)); 
+});
+ console.log(dataset);
+ createVis(dataset);
 });
 
-// var projectedLocation = projection([treeLon, treeLat]);
-// var circle = svg.append('circle')
+//let projectedLocation = projection([treeLon, treeLat]);
+//let circle = svg.append('circle')
 //   .attr('cx', projectedLocation[0])
 //   .attr('cy', projectedLocaiton[1])
 //   .attr('r', 10);
+
+function createVis(data) {
+  drawTrees(data);
+}
+
+
+function drawTrees(data) {
+  let circles = svg.selectAll('circle');
+  let updatedCircles = circles.data(data, d => d.TreeID);
+  // make circle 'pointers' for new datapoints
+  let enterSelection = updatedCircles.enter();
+  // now append actual new circle SVGs
+  let newCircles = enterSelection.append('circle')
+    .attr('r', 4)
+    .attr('cx', d => projection([d.Longitude, d.Latitude])[0])
+    .attr('cy', d => projection([d.Longitude, d.Latitude])[1])
+    .style('fill', 'steelblue')
+    .on("click", (d) => c(d.qSpecies));
+
+  updatedCircles.exit().remove();
+
+}
+
+// _________Helper Functions_________
+
+// Filter out any datasets missing crucial info
+function unususable(d) {
+  if (d.qSpecies.split('::')[1] == '') {
+    return true;
+  }
+  return !(d.Latitude && d.Longitude && d.qSpecies 
+    && d.qAddress && d.DBH && d.PlotSize);
+}
+
+function clean(d) {
+  d.TreeID    = +d.TreeID;
+  d.DBH       = +d.DBH;
+  d.Latitude  = +d.Latitude;
+  d.Longitude = +d.Longitude; 
+  // d.PlotSize relevent to normalize? 
+  return d;
+}
+
+function c(str) {
+  console.log(str);
+}
